a6b55e8bee4c73a4569fe8a0205d79c1ce7c645b
krupa950 <kumarkrupa96@gmail.com>
2023-03-03 19:41:33 +0530

    added message

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class sum {
     public static void main(String[] args) {
         int a=1,b=2;
-         System.out.println(a+b);
+         System.out.println("the sum is "+(a+b));
     }
     
 }
