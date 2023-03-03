bc24f92c4107f1e24d5d1a26d9a9ba2a407090b2
krupa950 <kumarkrupa96@gmail.com>
2023-03-03 19:34:13 +0530

    added sub file

---
+++ sub.java
@@ -0,0 +1,6 @@
+class sub{
+    public static void main(String[] args) {
+        int a=2,b=2;
+         System.out.println(a-b);
+    }
+}
---
+++ sum.java
@@ -0,0 +1,7 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=1,b=2;
+         System.out.println(a+b);
+    }
+    
+}
